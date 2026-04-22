b058d687b680a43b2a3cd18c9718abe11bf568d5
shuang-ai <2742322030@qq.com>
2026-04-22 20:33:33 +0800

    完成Socket类

---
+++ Socket.cc
@@ -0,0 +1,90 @@
+#include "Socket.h"
+#include "Logger.h"
+#include "InetAddress.h"
+
+#include <unistd.h>
+#include <sys/types.h>         
+#include <sys/socket.h>
+#include <strings.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+
+Socket::~Socket()
+{
+    close(sockfd_);
+}
+
+void Socket::bindAddress(const InetAddress &localaddr)
+{
+    if (0 != ::bind(sockfd_, (sockaddr*)localaddr.getSockAddr(), sizeof(sockaddr_in)))
+    {
+        LOG_FATAL("bind sockfd:%d fail \n", sockfd_);
+    }
+}
+
+void Socket::listen()
+{
+    if (0 != ::listen(sockfd_, 1024))
+    {
+        LOG_FATAL("listen sockfd:%d fail \n", sockfd_);
+    }
+}
+
+int Socket::accept(InetAddress *peeraddr)
+{
+    /**
+     * 1. accept函数的参数不合法
+     * 2. 对返回的connfd没有设置非阻塞
+     * Reactor模型 one loop per thread
+     * poller + non-blocking IO
+     */ 
+    sockaddr_in addr;
+    socklen_t len = sizeof addr;
+    bzero(&addr, sizeof addr);
+    // 设置SOCK_NONBLOCK：非阻塞
+    // 设置SOCK_CLOEXEC：执行 exec 时自动关闭 fd
+    int connfd = ::accept4(sockfd_, (sockaddr*)&addr, &len, SOCK_NONBLOCK | SOCK_CLOEXEC);
+    if (connfd >= 0)
+    {
+        peeraddr->setSockAddr(addr);
+    }
+    return connfd;
+}
+
+void Socket::shutdownWrite()
+{
+    if (::shutdown(sockfd_, SHUT_WR) < 0)
+    {
+        LOG_ERROR("shutdownWrite error");
+    }
+}
+
+void Socket::setTcpNoDelay(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof optval);
+}
+
+void Socket::setReuseAddr(bool on)
+{
+  // optval告诉内核是关闭(0)，还是开启(1)
+    int optval = on ? 1 : 0;
+    // SO_REUSEADDR表示允许重用本地地址
+    // SOL_SOCKET表示告诉内核我选择的是socket层，而不是TCP/UDP层
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+}
+
+void Socket::setReusePort(bool on)
+{
+    int optval = on ? 1 : 0;
+    // 允许多个 socket 绑定到同一个端口
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof optval);
+}
+
+void Socket::setKeepAlive(bool on)
+{
+    int optval = on ? 1 : 0;
+    // 如果客户端突然断电、网线断开、进程崩溃，服务端永远不知道连接已死。
+    // 当连接长时间空闲时，TCP 自动发送探测包，检测对端是否还活着。
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof optval);
+}
